2c74f9ff191299e201459260935434cf0d3ca328
Woonghee Cho <44767143+MaJangCowBoy@users.noreply.github.com>
2025-01-25 22:23:20 +0900

    code modified.

--- main_script_threads_1Q3Q.jl
+++ main_script_threads_1Q3Q.jl
@@ -55,29 +55,30 @@ Threads.@threads for i in 1:npar
 
     #? define LSWT part ?#
     #* 3Q LSWT part.
-    swt_3Q = [];
+    sys_3Q = [];  swt_3Q = [];
     for Bi in B1
       sysi = define_system(CoTa3S6,"3Q",J1,Bi,J2,J3,Jc1,Jc2,Kz,(3,3,1));
       keyword = "3Q";  sysi = system_initialize(sysi, keyword, J1);
       measure = ssf_perp(sysi; formfactors);  swti = SpinWaveTheory(sysi; measure);
-      global swt_3Q = [swt_3Q; swti];
+      swt_3Q = [swt_3Q; swti];  sys_3Q = [sys_3Q; sysi];
     end
     #* 1Q LSWT part.
-    swt_1Q = [];
+    sys_1Q = [];  swt_1Q = [];
     for key in ["1Q_1", "1Q_2", "1Q_3"]
       sysi = define_system(CoTa3S6,"1Q",J1,B1,J2,J3,Jc1,Jc2,Kz,(3,3,1));
       sysi = system_initialize(sysi, key, J1);
       measure = ssf_perp(sysi; formfactors);  swti = SpinWaveTheory(sysi; measure);
-      global swt_1Q = [swt_1Q; swti];
+      swt_1Q = [swt_1Q; swti];  sys_1Q = [sys_1Q; sysi];
     end
     #? define LSWT part ?#
 
     #? 2D: calculate intensities and store the data ?#
-    data2D_3Q = zeros(Float64,length(B1),N1,N2);  data2D_1Q = zeros(Float64,N1,N2);
+    data2D_3Q = NaN * zeros(Float64,length(B1),N1,N2);  data2D_1Q = NaN * zeros(Float64,N1,N2);
 
     for (k,swt) in enumerate(swt_3Q)
       try
-        res = intensities(swt, qgrid; energies, kernel);  global data2D_3Q[k,:,:] = res.data[1,:,:];
+        res = intensities(swt, qgrid; energies, kernel);  
+        data2D_3Q[k,:,:] = res.data[1,:,:];
       catch
         println("err in 3Q LSWT for 2D, B1 = ", B1[k], " J2 = ", j2, " Jc2 = ", jc2);
       end
@@ -85,50 +86,58 @@ Threads.@threads for i in 1:npar
 
     for (k,swt) in enumerate(swt_1Q)
       try
-        res = intensities(swt, qgrid; energies, kernel);  global data2D_1Q[:,:] += res.data[1,:,:];
+        res = intensities(swt, qgrid; energies, kernel);  
+        if k == 1  data2D_1Q[:,:]  = res.data[1,:,:];
+        else       data2D_1Q[:,:] += res.data[1,:,:];  end
       catch
-        println("err in 1Q LSWT for 2D, B1 = ", B1[k], " J2 = ", j2, " Jc2 = ", jc2);
+        println("err in 1Q LSWT for 2D,", " J2 = ", j2, " Jc2 = ", jc2);
       end
     end
     #? 2D: calculate intensities and store the data ?#
 
     #? 1D: calculate intensities and store the data ?#
-    data1Da_3Q = zeros(Float64,length(B1),N1);  data1Da_1Q = zeros(Float64,N1);
-    data1Db_3Q = zeros(Float64,length(B1),N2);  data1Db_1Q = zeros(Float64,N2);
+    data1Da_3Q = NaN * zeros(Float64,length(B1),N1);  data1Da_1Q = NaN * zeros(Float64,N1);
+    data1Db_3Q = NaN * zeros(Float64,length(B1),N2);  data1Db_1Q = NaN * zeros(Float64,N2);
     for (k,swt) in enumerate(swt_3Q)
       try
-        res1 = intensities(swt, qpath1; energies, kernel);  global data1Da_3Q[k,:] = res1.data[1,:];
-        res2 = intensities(swt, qpath2; energies, kernel);  global data1Db_3Q[k,:] = res2.data[1,:];
+        res1 = intensities(swt, qpath1; energies, kernel);  
+        data1Da_3Q[k,:] = res1.data[1,:];
+        res2 = intensities(swt, qpath2; energies, kernel);  
+        data1Db_3Q[k,:] = res2.data[1,:];
       catch
         println("err in 3Q LSWT for 1D, B1 = ", B1[k], " J2 = ", j2, " Jc2 = ", jc2);
       end
     end
     for (k,swt) in enumerate(swt_1Q)
       try
-        res1 = intensities(swt, qpath1; energies, kernel);  global data1Da_1Q[:] += res1.data[1,:];
-        res2 = intensities(swt, qpath2; energies, kernel);  global data1Db_1Q[:] += res2.data[1,:];
+        res1 = intensities(swt, qpath1; energies, kernel);  
+        if k == 1  data1Da_1Q[:]  = res1.data[1,:];
+        else       data1Da_1Q[:] += res1.data[1,:];  end
+        res2 = intensities(swt, qpath2; energies, kernel);  
+        if k == 1  data1Db_1Q[:] += res2.data[1,:];
+        else       data1Db_1Q[:] += res2.data[1,:];  end
       catch
-        println("err in 1Q LSWT for 1D, B1 = ", B1[k], " J2 = ", j2, " Jc2 = ", jc2);
+        println("err in 1Q LSWT for 1D,", " J2 = ", j2, " Jc2 = ", jc2);
       end
     end
     #? 1D: calculate intensities and store the data ?#
     
     #? data saving part ?#
-    tail = @sprintf("B1_%+.2f_%+.2f_J2_%+.2f_Jc1_%+.2f_Jc2_%+.2f",B1[1],B1[end],j2,jc1,jc2);
+    tail = @sprintf("B1_%+.2f_%+.2f_J2_%+.2f_Jc1_%+.2f_Jc2_%+.2f",b1[1],b1[end],j2,jc1,jc2);
     tail = replace(tail,"." => "p","-" => "M","+" => "P");
     h5name  = @sprintf("data_h5file_threads_1Q3Q_%s.h5",tail);
 
-    if any(isnan, data2D_3Q) || any(isnan, data2D_1Q) || any(isnan, data1Da_3Q) || any(isnan, data1Db_3Q) || any(isnan, data1Da_1Q) || any(isnan, data1Db_1Q)
-      h5name = replace(h5name, ".h5" => "_nan.h5");
-    end
+#     if any(isnan, data2D_3Q) || any(isnan, data2D_1Q) || any(isnan, data1Da_3Q) || any(isnan, data1Db_3Q) || any(isnan, data1Da_1Q) || any(isnan, data1Db_1Q)
+#       h5name = replace(h5name, ".h5" => "_nan.h5");
+#     end
     
     fid = h5open(h5name,"w");
     write(fid,"j2",j2);  write(fid,"jc1",jc1);  write(fid,"jc2",jc2);
     write(fid,"range1",range1);  write(fid,"norm1",norm1);
     write(fid,"range2",range2);  write(fid,"norm2",norm2);
-    write(fid, "data2D_3Q", data2D_3Q);  write(fid, "data2D_1Q", data2D_1Q);
-    write(fid,"data1Da_3Q", data2D_3Q);  write(fid,"data1Db_3Q", data2D_3Q);
-    write(fid,"data1Da_1Q", data2D_1Q);  write(fid,"data1Db_1Q", data2D_1Q);
+    write(fid, "data2D_3Q",  data2D_3Q);  write(fid, "data2D_1Q",  data2D_1Q);
+    write(fid,"data1Da_3Q", data1Da_3Q);  write(fid,"data1Db_3Q", data1Db_3Q);
+    write(fid,"data1Da_1Q", data1Da_1Q);  write(fid,"data1Db_1Q", data1Db_1Q);
     close(fid);
     #? data saving part ?#
   end
